fa42fad546e03f5c00f5db90173ccb51090723b4
ViktoriyaKo <kozlova_viktoriy@mail.ru>
2024-09-29 14:31:29 +0200

    feat: test template

--- src/packages/edit/templates/simple/simple2/Resume.tsx
+++ src/packages/edit/templates/simple/simple2/Resume.tsx
@@ -2,10 +2,11 @@ import { Content, Aside, ResumeWrapper } from '../components';
 
 const Resume = () => {
   return (
-    <ResumeWrapper style="simple2">
-      <Content style="simple2"/>
-      <Aside style="simple2"/>
-    </ResumeWrapper>
+    <div>test</div>
+    // <ResumeWrapper style="simple2">
+    //   <Content style="simple2"/>
+    //   <Aside style="simple2"/>
+    // </ResumeWrapper>
   );
 };
 
